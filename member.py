9606f5de47242fb8190f22dc281930a5a2804a52
Kieran Hunt <kieran.hunt92@gmail.com>
2015-10-14 22:01:50 +0200

    Changing to local imports

--- member.py
+++ member.py
@@ -1,6 +1,6 @@
-from rhodesrifleclub import db
 
 class Member(db.Model):
+    from rhodesrifleclub import db
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
     first_name = db.Column(db.String(120))
