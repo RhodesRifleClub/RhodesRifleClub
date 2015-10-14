a10d2fb9a872ef1743e1f84110ca02e37cfc8bf9
Kieran Hunt <kieran.hunt92@gmail.com>
2015-10-14 21:38:50 +0200

    Changing user to member.

--- rhodesrifleclub.py
+++ rhodesrifleclub.py
@@ -1,27 +1,16 @@
 from flask import Flask
 from flask import jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
+from member import Member
 
 application = Flask(__name__)
 application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rhodesrifleclub.db'
 db = SQLAlchemy(application)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
 @application.route("/users")
-def users():
-    users = User.query.all()
-    return jsonify(users = [user.username for user in users])
+def members():
+    members = Member.query.all()
+    return jsonify(users = [member.username for member in members])
 
 @application.route("/")
 def hello():
