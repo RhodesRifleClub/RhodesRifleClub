61f70bc80d1540022ffda4b21a62523b884c7581
Kieran Hunt <kieran.hunt92@gmail.com>
2015-10-15 06:43:51 +0200

    Refactoring Member back into main file

--- rhodesrifleclub.py
+++ rhodesrifleclub.py
@@ -1,11 +1,25 @@
 from flask import Flask
 from flask import jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
-from member import Member
 
 application = Flask(__name__)
 application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rhodesrifleclub.db'
 db = SQLAlchemy(application)
+
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    first_name = db.Column(db.String(120))
+    last_name = db.Column(db.String(120))
+
+    def __init__(self, email, first_name, last_name):
+        self.username = username
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __repr__(self):
+        return '%r %r' % self.first_name, self.last_name
 
 @application.route("/members.json")
 def members():
